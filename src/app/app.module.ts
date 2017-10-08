1fa031d767f90fc8eea5463be6d2c36c661c6f1b
Marcos Frank <marcos.frank@neoris.com>
2017-10-08 12:18:24 -0300

    - Routing funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { EjerciciosComponent } from './ejercicios/ejercicios.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const appRoutes : Routes = [
+  { path: 'welcome', component: WelcomeComponent},
+  { path: 'ejercicios', component: EjerciciosComponent},
+  { path: '', redirectTo:'welcome' ,pathMatch:'full'},
+  { path: '**', redirectTo:'welcome' ,pathMatch:'full'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EjerciciosComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
