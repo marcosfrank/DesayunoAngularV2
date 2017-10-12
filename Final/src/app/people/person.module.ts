299878171d4f118cbece39d1fe75ff1411047724
Marcos Frank <marcos.frank@neoris.com>
2017-10-12 00:00:10 -0300

    - Routes del modulo de personas definidos como constante

--- Final/src/app/people/person.module.ts
+++ Final/src/app/people/person.module.ts
@@ -6,17 +6,19 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { PersonMoodComponent } from './shared/person-mood.component';
 import { PersonDetailComponent } from './person-detail.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes : Routes = [
+    { path: 'people', component: PersonListComponent},
+    { path: 'people/detail/:id', component: PersonDetailComponent}
+];
 
 @NgModule({
   imports: [
     HttpClientModule,
     FormsModule,
     CommonModule,
-    RouterModule.forChild([
-      { path: 'people', component: PersonListComponent},
-      { path: 'people/detail/:id', component: PersonDetailComponent}
-    ])
+    RouterModule.forChild(routes)
   ],
   declarations: [
     PersonListComponent,
